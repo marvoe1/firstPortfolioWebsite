37cb0905e085d6dcaa201f97bad0c6cf9d55f4c4
marvoe1 <ekpenyongmarvel@gmail.com>
2024-02-18 17:58:21 +0100

    second commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -6,39 +6,39 @@ import Link from "next/link";
 import Image from "next/image"; //because to use svg we need to import Image to display the svg in Nextjs
 
 const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  // const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault(); //so that the page doesnt get reloaded when the form is submitted
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault(); //so that the page doesnt get reloaded when the form is submitted
+  //   const data = {
+  //     email: e.target.email.value,
+  //     subject: e.target.subject.value,
+  //     message: e.target.message.value,
+  //   };
+  //   const JSONdata = JSON.stringify(data);
+  //   const endpoint = "/api/send";
 
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application",
-      },
-      body: JSONdata,
-    };
+  //   const options = {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application",
+  //     },
+  //     body: JSONdata,
+  //   };
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-    // console.log(resData)
+  //   const response = await fetch(endpoint, options);
+  //   const resData = await response.json();
+  //   // console.log(resData)
 
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
-  };
+  //   if (response.status === 200) {
+  //     console.log("Message sent.");
+  //     setEmailSubmitted(true);
+  //   }
+  // };
 
   return (
-    <section className="grid md:grid-cols-2 my-12 md-:my-12 py-24 gap-4 relative">
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-xl absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2"></div>
+    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full md:h-80 md:w-80 z-0 blur-xl absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">Let's connect</h5>
         <p className="text-[#ADB7BE] mb max-w-md">
@@ -60,7 +60,7 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col" onSubmit={handleSubmit}>
+        <form className="flex flex-col">
           <div className="mb-6">
             <label
               htmlFor="email"
@@ -113,12 +113,12 @@ const EmailSection = () => {
           >
             Send Message
           </button>
-          {
+          {/* {
             emailSubmitted && (
             <p className="text-green-500 text-sm mt-2">
               Email sent successfully
             </p>)
-          }
+          } */}
         </form>
       </div>
     </section>
